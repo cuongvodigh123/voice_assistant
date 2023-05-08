9f2366a52aee3c6799a2f6a8fb48b1e214030b6a
cuongvodigh123/flappybird_python <leumaccuong@gmail.com>
2023-05-08 10:22:37 +0700

    thay đổi tiếng viêt

--- helo.py
+++ helo.py
@@ -16,26 +16,27 @@ def main():  # I know main function isn't required in py
 
 
     engine = pyttsx3.init()
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[1].id)
     engine.setProperty('rate', 150)
     engine.setProperty('volume', 1.0)
     engine.runAndWait()
-
     def speak(audio):
         engine.say(audio)
         engine.runAndWait()
 
     speak("Tôi là chatbot")
-    user = "adam"
+    user = "Cương"
     # user = input("type your name: ")
 
     def greet_user():
         hour = datetime.datetime.now().hour
         if hour >= 0 and hour < 12:
-            speak(f"GOOD MORNING {user}")
+            speak(f"Chào buổi sáng thưa ngài {user}")
         elif hour > 12 and hour < 17:
-            speak(f"GOOD AFTERNOON {user}")
+            speak(f"chào buổi chiều thưa ngài {user}")
         else:
-            speak(f"GOOD EVENING {user}")
+            speak(f"chào buổi tối thưa ngài {user}")
 
     def take_user_cmd():
         r = sr.Recognizer()
@@ -60,54 +61,61 @@ def main():  # I know main function isn't required in py
             if True:
                 query = take_user_cmd().lower()
 
+                if "một ngày" in query and ("đẹp trời" in query or "tồi tệ" in query):
+                    try:
+                        speak("Tôi chỉ là 1 công cụ máy tính, tôi không thể biết được hôm nay như thế nào")
+                        speak("bạn có muốn nói một thứ gì khác không")
+                        robot()
+                    except:
+                        speak("Lỗi không xác định")
                 if "mở notepad" in query:
                     try:
-                        speak("opening notepad")
+                        speak("mở nốt bát")
                         subprocess.Popen(["notepad.exe"])
                     except:
-                        speak("Unknown error occurred")
+                        speak("Lỗi không xác định")
 
                 elif "play on youtube" in query:
                     try:
-                        speak("What do you want to play on youtube?")
+                        speak("bạn muốn mở gì ở youtube?")
                         req = take_user_cmd().lower()
                         kit.playonyt(req)
                     except:
-                        speak("Unknown error occurred")
+                        speak("Lỗi không xác định")
 
                 elif "thời gian" in query:
                     try:
                         speak(datetime.date.today())
                         speak(time.strftime("%H:%M:%S", time.localtime()))
                     except:
-                        speak("Unknown error occurred")
+                        speak("Lỗi không xác định")
 
                 elif "mở thư mục" in query:
                     try:
-                        speak("opening file explorer")
+                        speak("mở thư mục")
                         os.startfile('explorer.exe')
                     except:
-                        speak("Unknown error occurred")
+                        speak("Lỗi không xác định")
 
                 elif "mở máy tính" in query:
                     try:
-                        speak("opening calculater")
+                        speak("đang mở máy tính")
                         os.startfile('calc.exe')
                     except:
-                        speak("Unknown error occurred")
+                        speak("Lỗi không xác định")
 
                 elif "mở cài đặt" in query:
                     try:
-                        speak("opening settings")
+                        speak("đang mở cài đặt")
                         os.system("start ms-settings:")
                     except:
-                        speak("Unknown error occurred")
+                        speak("Lỗi không xác định")
                 elif "mở văn bản" in query:
                     try:
                         speak("opening word")
                         os.startfile(r'C:\Users\ADMIN\Desktop\Word 2016.lnk')
                     except:
-                        speak("Unknown error occurred")
+                        speak("Lỗi không xác định")
                                 
                 elif "mở trình duyệt" in query:
                     try:
@@ -116,7 +124,7 @@ def main():  # I know main function isn't required in py
                         os.startfile("C:\Program Files (x86)\Google\Chrome\Application\chrome_proxy.exe")
                         
                     except:
-                        speak("Unknown error occurred")
+                        speak("Lỗi không xác định")
 
                 elif "mở thư điện tử" in query:
                     try:
@@ -124,7 +132,7 @@ def main():  # I know main function isn't required in py
                         webbrowser.register('chrome', None, webbrowser.BackgroundBrowser(chrome_path))
                         webbrowser.get('chrome').open('https://mail.google.com/mail/')
                     except:
-                        speak("Unknown error occurred")
+                        speak("Lỗi không xác định")
                 elif "mở zalo" in query:
                     try:
                         
@@ -132,33 +140,52 @@ def main():  # I know main function isn't required in py
                         webbrowser.register('chrome', None, webbrowser.BackgroundBrowser(chrome_path))
                         webbrowser.get('chrome').open('https://chat.zalo.me/')
                     except:
-                        speak("Unknown error occurred")
+                        speak("Lỗi không xác định")
                 
-                else:
-                        speak("I found this on the web")
+                elif "mở facebook" in query:
+                    try:
+                        chrome_path = "C:\Program Files (x86)\Google\Chrome\Application\chrome_proxy.exe"
+                        webbrowser.register('chrome', None, webbrowser.BackgroundBrowser(chrome_path))
+                        webbrowser.get('chrome').open('https://www.facebook.com/')
+                    except:
+                        speak("Lỗi không xác định")
+                
+                elif "hãy tìm kiếm" in query and "trên mạng" in query:
+                    try:
+                        speak("tôi tìm được thứ này trên web")
+                        chrome_path = "C:\Program Files (x86)\Google\Chrome\Application\chrome_proxy.exe"
+                        webbrowser.register('chrome', None, webbrowser.BackgroundBrowser(chrome_path))
+                        url = search(query)[0]
                         for url in search(query):
                             print(url)
+                        webbrowser.get('chrome').open(url)
+                        
+                    except:
+                        speak("Lỗi không xác định")    
+                        
+                else:
+                    pass
 
     robot()
 
     while True:
-        speak("Would you like to run this tool again? \n")
+        speak("Bạn có thêm câu hỏi gì với tôi không ? \n")
 
         query = take_user_cmd().lower()
 
         if query == "có" or query == "đúng vậy":
-            speak("very well, say again what you want me to do")
+            speak("rất tốt, hãy hỏi tôi tiếp đi")
             robot()
-        elif query == "không" or query == "không phải bây giờ" :
-            speak("Cool thanks for using the programme")
+        elif query == "không" or query == "không phải bây giờ" or query == "nâu" or query == "lâu":
+            speak("cảm ơn vì đã sử dụng tôi")
             hour = datetime.datetime.now().hour
             if hour >= 22 and hour < 6:
-                speak(f"Good NIGHT{user}")
+                speak(f"chúc buổi tối tốt lành{user}")
             else:
-                speak(f"have a good day{user}")
+                speak(f"chúc một ngày mới an lành{user}")
             break
         else:
-            speak(print("That maybe beyond my abilities at the moment"))
+            speak(print("Điều đó có lẽ vượt quá khả năng của tôi vào lúc này"))
 
 
 main()
--- test.py
+++ test.py
@@ -1,14 +1,80 @@
-import wikipedia
-from googlesearch import search
+def main():  # I know main function isn't required in py
+    import pyttsx3
+    import speech_recognition as sr
+    import datetime
+    import pywhatkit as kit
+    from AppOpener import run
+    import time
+    import requests
+    import smtplib
+    import wikipedia
+    import googlesearch
+    from googlesearch import search
+    import subprocess
+    import os
+    import webbrowser
 
-# Tìm kiếm thông tin về Python trên Wikipedia
-# query = "Python"
-# results = wikipedia.search(query)
-# if results:
-#     page = wikipedia.page(results[0])
-#     print(page.summary)
+    engine = pyttsx3.init()
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[1].id)
+    engine.setProperty('rate', 150)
+    engine.setProperty('volume', 1.0)
+    engine.runAndWait()
+    def speak(audio):
+        engine.say(audio)
+        engine.runAndWait()
+    def take_user_cmd():
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            print("..........LISTENING...........")
+            r.pause_threshold = 1
+            audio = r.listen(source)
+        try:
+            print(".......Recognizing........")
+            query = r.recognize_google(audio, language='vi-en')
+            print(f"User said: {query}\n")
+        except Exception:
+            print("Sorry, I didn't get that, I'd appreciate if you could try again ")
+            speak("hãy nói lại để tôi rõ hơn ")
+            query = "hãy nói lại"
+        return query
+    def robot():
+        if __name__ == "__main__":
 
-# Tìm kiếm trên Google
-query = "người việt nam"
-for url in search(query):
-    print(url)
+            if True:
+                query = "hãy tìm kiếm người việt nam trên mạng"
+                if "hãy tìm kiếm" in query and "trên mạng" in query:
+                    try:
+                        speak("tôi tìm được thứ này trên web")
+                        chrome_path = "C:\Program Files (x86)\Google\Chrome\Application\chrome_proxy.exe"
+                        webbrowser.register('chrome', None, webbrowser.BackgroundBrowser(chrome_path))
+                        # url = str(search(query)[0])
+                        # for url in search(query):
+                        #     print(url)
+                        webbrowser.get('chrome').open("https://www.facebook.com/")
+                        
+                    except:
+                        speak("Lỗi không xác định")    
+    robot()
+
+    while True:
+        speak("Bạn có thêm câu hỏi gì với tôi không ? \n")
+
+        query = take_user_cmd().lower()
+
+        if query == "có" or query == "đúng vậy":
+            speak("rất tốt, hãy hỏi tôi tiếp đi")
+            robot()
+        elif query == "không" or query == "không phải bây giờ" or query == "nâu" or query == "lâu":
+            speak("cảm ơn vì đã sử dụng tôi")
+            hour = datetime.datetime.now().hour
+            if hour >= 22 and hour < 6:
+                speak(f"chúc buổi tối tốt lành{user}")
+            else:
+                speak(f"chúc một ngày mới an lành{user}")
+            break
+        else:
+            speak(print("Điều đó có lẽ vượt quá khả năng của tôi vào lúc này"))
+
+
+main()
